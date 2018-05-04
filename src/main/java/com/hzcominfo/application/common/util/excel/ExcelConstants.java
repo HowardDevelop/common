e54af06b9b14dd9a398e77076c4e77df15166b38
HowardDevelop <905674397@qq.com>
2018-05-04 16:49:07 +0800

    add

---
+++ src/main/java/com/hzcominfo/application/common/util/excel/ExcelConstants.java
@@ -0,0 +1,9 @@
+package com.hzcominfo.application.common.util.excel;
+
+public class ExcelConstants {
+    public static String XLSX="xlsx";
+    public static String XLS="xls";
+    public static String CSV="csv";
+    public static String TXT="txt";
+
+}
